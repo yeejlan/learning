a758b58273903abbdcc59a8515b53a501fbf7b8d
louis.yu <yf_hello@163.com>
2015-08-13 16:59:18 +0800

    add --preserve-root for chown and chmod

--- golang/chg2apache/chg2apache.go
+++ golang/chg2apache/chg2apache.go
@@ -13,14 +13,14 @@ func main(){
 	for _, value := range flag.Args() {
 		var out []byte
 		var err error
-		out, err = exec.Command("chown", "apache.apache", "-R", value).CombinedOutput()	
+		out, err = exec.Command("chown", "apache.apache", "-R", "--preserve-root", value).CombinedOutput()	
 		fmt.Printf("%s", out);
 		if err != nil {
 			fmt.Printf("chown: %s\r\n", err)
 			break
 		}
 
-                out, err = exec.Command("chmod", "g+w", "-R", value).CombinedOutput()
+                out, err = exec.Command("chmod", "g+w", "-R", "--preserve-root", value).CombinedOutput()
                 fmt.Printf("%s", out);
                 if err != nil {
 			fmt.Printf("chmod: %s\r\n", err)
